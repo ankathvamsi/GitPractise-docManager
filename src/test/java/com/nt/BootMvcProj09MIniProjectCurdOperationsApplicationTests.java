de55df696a7f74cd99a6d5f5cbf8587eb739ad20
Ankath Vamshi <ankathvamsi@gmail.com>
2026-08-10 16:37:02 +0530

    first commit

---
+++ src/test/java/com/nt/BootMvcProj09MIniProjectCurdOperationsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.nt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class BootMvcProj09MIniProjectCurdOperationsApplicationTests {
+	//Bug fix 130
+	@Test
+	void contextLoads() {
+	}
+
+}
